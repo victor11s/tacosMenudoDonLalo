8a1897c2424af2ff85fd7157a1922e4b09848f91
victor11s <vic.manuel35@gmail.com>
2022-10-22 12:20:46 -0500

    REINSTALACION DE SCRIPTS

--- tdml/src/App.js
+++ tdml/src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { Row, Col } from 'react-bootstrap'
+
+
+
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Row>
+        <Col>1 of 2</Col>
+        <Col>2 of 2</Col>
+      </Row>
+
+      <Row>
+        <Col>1 of 3</Col>
+        <Col>2 of 3</Col>
+        <Col>3 of 3</Col>
+      </Row>
+
     </div>
+
   );
-}
+
+
+
+
+
+};
 
 export default App;
