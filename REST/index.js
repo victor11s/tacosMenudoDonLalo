53c7db8229f9847104ddf5ed6fff6932b80c9bbb
deniseuan <99851467+deniseuan@users.noreply.github.com>
2022-11-12 00:35:02 -0600

    API aun funcional
    Se procede a incorporar mysql

--- REST/index.js
+++ REST/index.js
@@ -1,6 +1,14 @@
+//Configuración de express
 const express = require('express'); //llamamos a Express
 const app = express();
-const port = process.env.PORT || 3001  // establecemos nuestro puerto
+const port = process.env.PORT || 3001;  // establecemos nuestro puerto
+
+//Importamos mysql
+//const mysql = require('mysql');
+
+//Importamos body-parser para analizar los cuerpos de las solicitudes entrantes como middleware.
+const bodyParser = require('body-parser');
+
 
 app.get('/', function (req, res) {
     res.send('Saludos desde express');
