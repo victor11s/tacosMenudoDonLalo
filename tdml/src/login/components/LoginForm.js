83a9291bcd77c35d854141b7f9a9f09ae5c2bc69
deniseuan <99851467+deniseuan@users.noreply.github.com>
2022-11-13 12:41:27 -0600

    Update LoginForm.js

--- tdml/src/login/components/LoginForm.js
+++ tdml/src/login/components/LoginForm.js
@@ -62,35 +62,3 @@ export default class LoginForm extends Component {
     }
 }
 
-/** 
- 
- import React from 'react'
-import { Button, Form } from 'react-bootstrap'
-
-export default function LoginForm() {
-    return (
-        <div>
-            <Form>
-                <legend>
-                    Iniciar Sesión
-                </legend>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Correo electrónico</Form.Label>
-                    <Form.Control type="email" placeholder="Introduce tu email" value={"prueba@udem.edu"} />
-                    <Form.Text className="text-muted">
-                        No compartiremos tu correo con nadie más.
-                    </Form.Text>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Contraseña</Form.Label>
-                    <Form.Control type="password" placeholder="Contraseña" value={666} />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Iniciar Sesión
-                </Button>
-            </Form>
-        </div>
-    )
-}
- 
-*/
