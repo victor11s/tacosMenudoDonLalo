93cea2c3483bb336338332bc1bf6263e2397275d
deniseuan <99851467+deniseuan@users.noreply.github.com>
2022-11-14 04:56:00 -0600

    LOGIN FUNCIONAL

--- REST/routes/login.js
+++ REST/routes/login.js
@@ -14,12 +14,37 @@ router.post('/login', (req, response) => {
         db.query(sqlQuery, [nombre_usuario, password], (error, result) => {
             //res.json({ message: 'Vas a iniciar sesión' });
             if (error) throw error;
+
             if (result.length > 0) {
                 // Authenticate the user
-                response.send("Usuario validado")
-                
+                response.send(true);
             } else {
-                response.send('Incorrect Username and/or Password!');
+                response.send(false);
+            }
+        });
+
+
+    } else {
+        response.send('Introduzca un nombre de usuario y contraseña!');
+        response.end();
+    }
+
+})
+
+router.post('/signin', (req, response) => {
+    const nombre_usuario = req.body.nombre_usuario;
+    const password = req.body.password;
+    console.log("Datos: "+nombre_usuario +" && "+ password)
+    if (nombre_usuario != undefined && password != undefined) {
+        const sqlQuery = "CALL RegistroUsuario((?),(?))";
+        db.query(sqlQuery, [nombre_usuario, password], (error, result) => {
+            // console.log(result); 
+            if (error) throw error;
+            if (result[0] == undefined) { //mysl retorna undefined si se insertó correctamente.
+                // Authenticate the user
+                response.send(true);
+            } else {
+                response.send(false);
             }
         });
 
--- tdml/src/login/components/SigninForm.js
+++ tdml/src/login/components/SigninForm.js
@@ -1,32 +1,124 @@
 import React, { Component } from 'react'
-import { Button, Form } from 'react-bootstrap'
+import Axios from 'axios';
+import { Button, Form, Modal } from 'react-bootstrap'
+
 
 export default class SigninForm extends Component {
+
+    ligaAxios = 'http://localhost:3001/api/';
+
+    state = {
+        form: {
+            "nombre_usuario": "",
+            "password": "",
+            "password_confirm": "",
+        },
+        showModal: false
+    }
+
+    handleChange = async e => {
+        await this.setState({
+            form:
+            {
+                ...this.state.form,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+
+    handleSubmit = async e => {
+        e.preventDefault();
+        if (this.state.form.nombre_usuario !== "" && this.state.nombre_usuario !== "") {
+            await Axios.post((this.ligaAxios + 'signin'),
+                {
+                    nombre_usuario: this.state.form.nombre_usuario,
+                    password: this.state.form.password
+                }).then(async res => {
+                    let authorization = res.data;
+                    //console.log(authorization);
+                    if (authorization) {
+                        //alert("Registro de usuario exitoso")
+                        this.handleShow()
+                        //window.open("/login", "_self");
+                    } else {
+                        // this.handleShow();
+                        // let modal = document.getElementById("modal");
+                        // //modal.show = true;
+                        // console.log(modal);
+                        alert("El nombre de usuario ya está registrado")
+                    }
+
+                })
+
+        } else {
+            alert("Llene correctamente todos los campos")
+        }
+    }
+
+    handleClose = async e => {
+        await this.setState({
+            showModal: false
+        });
+        window.open("/login", "_self");
+    }
+
+    handleShow = async e => {
+        await this.setState({
+            showModal: true
+        });
+    }
+
     render() {
         return (
-            <Form>
-                <legend>
-                    Registrate
-                </legend>
-                <Form.Group className="mb-3" controlId="formEmail">
-                    <Form.Label>Correo electrónico</Form.Label>
-                    <Form.Control type="email" placeholder="Introduce tu email" value={"prueba@udem.edu"} />
-                    <Form.Text className="text-muted">
-                        No compartiremos tu correo con nadie más.
-                    </Form.Text>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formPassword1">
-                    <Form.Label>Contraseña</Form.Label>
-                    <Form.Control type="password" placeholder="Contraseña" value={666} />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formPassword2">
-                    <Form.Label>Repetir contraseña</Form.Label>
-                    <Form.Control type="password" placeholder="Repetir contraseña" value={666} />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Registrarme
-                </Button>
-            </Form>
+            <>
+                {/* Modal mostrado en caso de registro existoso */}
+                <Modal
+                    show={this.state.showModal}
+                    size="lg"
+                    aria-labelledby="contained-modal-title-vcenter"
+                    centered>
+                    <Modal.Header closeButton>
+                        <Modal.Title id="contained-modal-title-vcenter">
+                            Usuario registrado
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <p>
+                            El registro de usuario ha sido exitoso.
+                        </p>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={this.handleClose}>Continuar</Button>
+                    </Modal.Footer>
+                </Modal>
+                {/* Termina modal oculto */}
+                <Form onSubmit={this.handleSubmit}>
+                    <legend>
+                        Registrate
+                    </legend>
+                    <Form.Group className="mb-3" controlId="formEmail">
+                        <Form.Label>Correo electrónico</Form.Label>
+                        <Form.Control name="nombre_usuario" type="email" placeholder="Introduce tu email"
+                            defaultValue={"prueba@udem.edu"} onChange={this.handleChange} required />
+                        <Form.Text className="text-muted">
+                            No compartiremos tu correo con nadie más.
+                        </Form.Text>
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formPassword1">
+                        <Form.Label>Contraseña</Form.Label>
+                        <Form.Control name="password" type="password" placeholder="Contraseña"
+                            defaultValue={666} onChange={this.handleChange} required />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formPassword2">
+                        <Form.Label>Repetir contraseña</Form.Label>
+                        <Form.Control name="password_confirm" type="password" placeholder="Repetir contraseña"
+                            defaultValue={666} onChange={this.handleChange} required />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Registrarme
+                    </Button>
+                </Form>
+            </>
         )
     }
 }
