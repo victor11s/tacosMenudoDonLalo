2257b3c02474f20afdb99324db78601d173bc33f
victor11s <vic.manuel35@gmail.com>
2022-11-30 16:44:47 -0600

    ajustes

--- tdml/src/landingPage/LandingPage.js
+++ tdml/src/landingPage/LandingPage.js
@@ -6,13 +6,16 @@ import Feature1 from './components/feature1'
 import DefaultNavbar from '../globalComponents/defaultNavbar';
 
 
+let fondo={  backgroundImage: `url('components/images/tacoFondo.jpg'})`  }
+
+
 
 export default class LandingPage extends Component {
     render() {
         return (
         <div className="TacosColor">
             <DefaultNavbar/>
-            <Container>
+            <Container className='FondoImagenP'>
             <Row>
                 <Col>
                 <Feature1/>
