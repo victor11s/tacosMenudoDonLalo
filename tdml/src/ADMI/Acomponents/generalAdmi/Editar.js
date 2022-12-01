11014c963051be368cbe00ddd1354d68b60b984e
victor11s <vic.manuel35@gmail.com>
2022-11-30 22:03:16 -0600

    ajustes

--- tdml/src/ADMI/Acomponents/generalAdmi/Editar.js
+++ tdml/src/ADMI/Acomponents/generalAdmi/Editar.js
@@ -1,13 +1,59 @@
 import React, { Component } from 'react'
-
-
+import Axios from 'axios';
 import { Card, ListGroup, Row, Col, Button, Form, Nav, Container, Tab, Tabs } from 'react-bootstrap'
 
 export default class Editar extends Component {
+
+    ligaAxios = 'http://localhost:3001/api/';
+
+    /* Insertar datos que se piden de la BD */
+    state = {
+        form: {
+            "nombre_usuario": "",
+            "password": "",
+        },
+        showModal: false
+    }
+
+    handleChange = async e => {
+        await this.setState({
+            form:
+            {
+                ...this.state.form,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+
+    handleSubmit = async e => {
+        e.preventDefault();
+        await Axios.post((this.ligaAxios + 'login'),
+            {
+                nombre_usuario: this.state.form.nombre_usuario,
+                password: this.state.form.password
+            }).then(async res => {
+                let authorization = res.data;
+                //console.log(authorization);
+                if (authorization) {
+                    localStorage.setItem("nombre_usuario", this.state.form.nombre_usuario);
+                    localStorage.setItem("logged", true);
+                    window.open("/", "_self");
+                } else {
+                    // this.handleShow();
+                    // let modal = document.getElementById("modal");
+                    // //modal.show = true;
+                    // console.log(modal);
+                    //alert("Nombre de usuario o contraseña inorrecta")
+                    this.handleShow();
+                }
+
+            })
+    }
+
     render() {
         return (
             <div>
-                
+
 
 
 
