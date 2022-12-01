4a41e90846d619419427f6fdaabed232156a0072
victor11s <vic.manuel35@gmail.com>
2022-12-01 17:43:59 -0600

    Modal

--- tdml/src/ADMI/Acomponents/generalAdmi/Editar.js
+++ tdml/src/ADMI/Acomponents/generalAdmi/Editar.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Axios from 'axios';
 
+import { Modal, Button } from 'react-bootstrap';
+
 
 export default class Editar extends Component {
 
@@ -12,7 +14,18 @@ export default class Editar extends Component {
             "nombre_usuario": "",
             "password": "",
         },
-        showModal: false
+            showModal: false
+  
+    }
+
+    onClickButton = e => {
+        e.preventDefault();
+        this.setState({ showModal: true });
+    }
+
+    onClose = e => {
+        e.preventDefault();
+        this.setState({ showModal: false });
     }
 
     handleChange = async e => {
@@ -61,7 +74,7 @@ export default class Editar extends Component {
                             <th scope="col">Costo</th>
                             <th scope="col">Stock</th>
                             <th scope="col">Opciones</th>
-                            
+
 
 
                         </tr>
@@ -72,7 +85,8 @@ export default class Editar extends Component {
                             <td>Rico taco de Frijol</td>
                             <td>$20</td>
                             <td>10</td>
-                            <td><button type="button" class="btn btn-primary m-1">Editar</button><button type="button" class="btn btn-danger">Eliminar</button></td>
+                            <td><button  className="btn btn-primary m-1" onClick={this.onClickButton}>Editar</button>
+                            <button type="button" class="btn btn-danger">Eliminar</button></td>
 
 
                         </tr>
@@ -80,7 +94,25 @@ export default class Editar extends Component {
                     </tbody>
 
                 </table>
+
+                <Modal open={this.state.showModal} onClose={this.onClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={this.handleClose}>
+                            Save Changes
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+
+
             </div>
+
         )
     }
 }
