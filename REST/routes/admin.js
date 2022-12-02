5caf0822985a65d909156b4810fe35781c192b0d
deniseuan <99851467+deniseuan@users.noreply.github.com>
2022-12-01 19:02:46 -0600

    programacion de insert producto

---
+++ REST/routes/admin.js
@@ -0,0 +1,68 @@
+const router = require('express').Router();
+const db = require('../db');
+
+router.post('/insert', function (req, response) {
+    const nombre = req.body.nombre;
+    const descripcion = req.body.descripcion;
+    const stock = req.body.stock;
+    const tipo = req.body.tipo;
+    const precio_unitario = req.body.precio_unitario;
+    const imagen = req.body.imagen;
+    
+    const noUndefinedFields = nombre != undefined && tipo != undefined && precio_unitario != undefined && imagen != undefined;
+    const noEmptyFields = nombre != "" && tipo != "" && precio_unitario != "" && imagen != "";
+
+    if (noUndefinedFields&&noEmptyFields) {
+        const sqlQuery = "INSERT INTO articulo(nombre, descripcion, stock, tipo, precio_unitario, imagen) VALUES ((?), (?), (?), (?), (?), (?));";
+        db.query(sqlQuery, [nombre, descripcion, stock, tipo, precio_unitario, imagen], (error, result) => {
+            //res.json({ message: 'Vas a iniciar sesión' });
+            console.log(result);
+            if (error) throw error;
+            if (result.affectedRows > 0) {
+                // Authenticate the insertion
+                response.send(true);
+            } else {
+                response.send(false);
+            }
+        });
+
+    } else {
+        response.send('Introduzca correctamente los datos');
+        //console.log([nombre, tipo, precio_unitario, imagen]);
+        response.end();
+    }
+});
+
+router.put('/update', function (req, response) {
+    const nombre = req.body.nombre;
+    const descripcion = req.body.descripcion;
+    const stock = req.body.stock;
+    const tipo = req.body.tipo;
+    const precio_unitario = req.body.precio_unitario;
+    const imagen = req.body.imagen;
+    
+    const noUndefinedFields = nombre != undefined && tipo != undefined && precio_unitario != undefined && imagen != undefined;
+    const noEmptyFields = nombre != "" && tipo != "" && precio_unitario != "" && imagen != "";
+
+    if (noUndefinedFields&&noEmptyFields) {
+        const sqlQuery = "UPDATE articulo SET nombre=(?), descripcion=(?), stock=(?), tipo=(?), precio_unitario=(?), imagen=(?)) WHERE id_;";
+        db.query(sqlQuery, [nombre, descripcion, stock, tipo, precio_unitario, imagen], (error, result) => {
+            //res.json({ message: 'Vas a iniciar sesión' });
+            console.log(result);
+            if (error) throw error;
+            if (result.affectedRows > 0) {
+                // Authenticate the insertion
+                response.send(true);
+            } else {
+                response.send(false);
+            }
+        });
+
+    } else {
+        response.send('Introduzca correctamente los datos');
+        //console.log([nombre, tipo, precio_unitario, imagen]);
+        response.end();
+    }
+});
+
+module.exports = router;
