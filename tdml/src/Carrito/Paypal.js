5099bbf49adf65c9868fbe2bf8a6fdad7f60a93b
victor11s <vic.manuel35@gmail.com>
2022-12-05 02:37:34 -0600

    boton paypal

---
+++ tdml/src/Carrito/Paypal.js
@@ -0,0 +1,22 @@
+import React from 'react'
+ 
+import PayPalCheckout from 'react-paypal-checkout-button'
+import 'react-paypal-checkout-button/dist/index.css'
+ 
+const Paypal = () => {
+  return (
+    <PayPalCheckout
+      clientId='AeIKI84FQ7jy_GvjZNXetrwL-PRIqLuJKFt-aI3jKqXvkKj2fJult-2RVtf03GN_Co4trcItzLvQvsx7'
+      amount={100}
+      currency='USD'
+      onSuccess={(data, order) => {
+        console.log(data, order)
+      }}
+      onError={(error) => {
+        console.log(error)
+      }}
+    />
+  )
+}
+ 
+export default Paypal
